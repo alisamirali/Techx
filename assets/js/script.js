660b3646fb3e5771da6931f5ff3941ad34727d02
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-07-10 13:42:12 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/* Navbar Toggle */
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+navToggleBtn.addEventListener("click", function() {
+    navbar.classList.toggle("active");
+    this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+    navbarLinks[i].addEventListener("click", function() {
+        navbar.classList.toggle("active");
+        navToggleBtn.classList.toggle("active");
+    });
+}
+
+/* Header */
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+});
